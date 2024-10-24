dad06a8386a348602362c128c96a3b7521abb212
Martí Espinosa <141586888+martiespinosa@users.noreply.github.com>
2024-10-24 17:41:41 +0200

    extra

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    music.changeTempoBy(input.acceleration(Dimension.X))
 })
 input.onButtonPressed(Button.B, function () {
     music.play(music.stringPlayable("C C5 C C5 C C5 C C5 ", 120), music.PlaybackMode.UntilDone)
+    music.changeTempoBy(input.acceleration(Dimension.X))
 })
 basic.forever(function () {
 	
