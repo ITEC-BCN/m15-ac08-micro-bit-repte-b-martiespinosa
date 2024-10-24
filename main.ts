19375ec7ff42f9c1168ee619f403e490a4a77ced
Martí Espinosa <141586888+martiespinosa@users.noreply.github.com>
2024-10-24 17:38:51 +0200

    music

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("C C5 C C5 C C5 C C5 ", 120), music.PlaybackMode.UntilDone)
+})
 basic.forever(function () {
 	
 })
